8182945691d32eacc3ee68cee65713e29be26861
iamfrodo <nandiniramakrishnan@cmu-845991.wv.cc.cmu.edu>
2014-09-04 23:02:33 -0400

    Started 1st example
    ReplaceProcess

---
+++ src/ReplaceProcess.java
@@ -0,0 +1,23 @@
+
+public class ReplaceProcess extends MigratableProcesses {
+	// TransactionalFileInputStream in
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suspend() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

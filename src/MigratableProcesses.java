7036a6864f9bc62912550b8e2fbaa93770a77d6f
shrkaa <shrkaa@yahoo.com>
2014-09-04 22:25:21 -0400

    First commit
    First commit- just testing

---
+++ src/MigratableProcesses.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public abstract class MigratableProcesses implements java.lang.Runnable,
+		java.io.Serializable {
+	private ArrayList<String> arguments;
+
+	public MigratableProcesses() {
+		arguments = new ArrayList<String>();
+	}
+
+	/**
+	 * Called before an object is serialized to allow the process to enter a
+	 * known safe state
+	 */
+	public abstract void suspend();
+
+	/**
+	 * This method produces a symbol string representation of the object by
+	 * printing the class name of the process as well as the original set of
+	 * arguments in which it was called
+	 */
+	@Override
+	public abstract String toString();
+	
+	
+	public ArrayList<String> getArguments()
+	{
+		return arguments;
+	}
+}
